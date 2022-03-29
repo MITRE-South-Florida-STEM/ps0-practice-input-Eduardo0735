d3ceb4f2bd8204c6b0ec837b3501a5919aec2c67
Eduardo0735 <102366713+Eduardo0735@users.noreply.github.com>
2022-03-29 02:30:31 +0000

    code

--- prac_input.py
+++ prac_input.py
@@ -1,13 +1,13 @@
 # please use the code below to run your code
 
 # replace the following line to prompt user for first number
-print('first question')
-
+print('Please enter the first number')
+x = int(input())
 # replace the following line to prompt user for second number
-print('second question')
-
+print('Please enter the second number')
+y = int(input())
 # replace the following line to compute exponent
-print('compute something')
+print(x, 'raised to', y , 'is')
 
 # replace the following line to print solution
-print('print solution')
+print( x ** y)
